95b42243f2941d08a1713cada1ee3a258f549ee8
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-09-28 20:38:27 +0300

    838

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,10 @@
 import { css } from '@emotion/css';
+import { CgMenu } from 'react-icons/cg';
+import { FaApple } from 'react-icons/fa';
 
 const black = '#101010';
 const white = '#fff';
+const gray = '#f2f2f2';
 
 export const Layout = ({ children }) => {
   const gridCss = css`
@@ -9,7 +12,7 @@ export const Layout = ({ children }) => {
     grid-template-areas:
       'header'
       'main-area';
-    grid-template-rows: 40px 1fr;
+    grid-template-rows: 60px 1fr;
 
     @media (min-width: 1024px) {
       grid-template-areas:
@@ -25,19 +28,32 @@ export const Layout = ({ children }) => {
     background-color: ${black};
     color: ${white};
     grid-area: header;
+    padding: 0 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   `;
 
   const mainAreaCss = css`
     grid-area: main-area;
   `;
 
+  const footerCss = css`
+    background-color: ${gray};
+    padding: 0 16px;
+  `;
+
   return (
     <div className={gridCss}>
-      <header className={headerCss}>x</header>
+      <header className={headerCss}>
+        <FaApple size="32"></FaApple>
+
+        <CgMenu size="32"></CgMenu>
+      </header>
 
       <div className={mainAreaCss}>
         {children}
-        <footer>x</footer>
+        <footer className={footerCss}>x</footer>
       </div>
     </div>
   );

2d14abd5f856e1367257152f8129831d0d644966
Icantwritecode97 <96843873+Icantwritecode97@users.noreply.github.com>
2022-10-24 22:10:10 +0300

    update

--- src/components/catalog/GridControls.jsx
+++ src/components/catalog/GridControls.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useIsMobile, useLocalStorage } from '../../hooks';
 
 const buttonClasses = `w-24 h-24 border-l border-zinc-400 flex justify-center items-center`;
 
 export const GridControls = ({ setPerRow = () => {} }) => {
-  const [itemsPerRow, setItemsPerRow] = useState('4/row');
+  const [itemsPerRow, setItemsPerRow] = useLocalStorage('perRow', '4/row');
+  const isMobile = useIsMobile();
 
   // de fiecare data cand itemsPerRow se schimba
   // ruleaza acest callback
@@ -12,6 +14,10 @@ export const GridControls = ({ setPerRow = () => {} }) => {
 
     setPerRow(perRow);
   }, [itemsPerRow, setPerRow]);
+
+  if (isMobile === true) {
+    return <></>;
+  }
 
   return (
     <ul className="flex border border-l-0 border-r-0 border-zinc-400">
